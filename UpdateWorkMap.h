da95ece74838076c9d2edad08268b9d5905eaf38
Vinicius Marino Calvo Torres de Freitas <vinimmbb@gmail.com>
2018-08-27 18:32:29 -0300

    Update UpdateWorkMap.h

--- UpdateWorkMap.h
+++ UpdateWorkMap.h
@@ -53,15 +53,20 @@ class WorkMap {
     void insert(WorkObj);
     bool exists(int index);
     const WorkObj& find(int index); // Throws exception if not found
-    bool remove(int index); // false if not removed, true otherwise
+    std::vector<WorkObj> remove_batch(std::vector<int> indexes);
+    std::vector<WorkObj> remove_batch_of_load(double& expected_load);
+    std::vector<WorkObj> remove_batch_of_load_to_proc(double& expected_load, int target_proc);
+    double calculate_total_load();
 
  private:
     std::unordered_map<int, WorkObj> all_objs;
     std::vector<WorkObj&> ordered_objs;
     std::vector<WorkGroup> frontiers;
     std::set<WorkObj> recent;
+    double my_total_load; // should be updated on remove
 
     void sort_objs();
+    bool remove(int index); // false if not removed, true otherwise
     void create_obj_naive_map(int n_objs, int* objs);
     void remove_from_all(const WorkObj& obj); // Should be overrided by specialized classes
 
